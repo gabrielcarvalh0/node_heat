e91d1098904d24b0eee65e4e2a374561cc6579f7
Gabriel Carvalho <gabrielcarvalho5107@gmail.com>
2022-10-26 21:58:05 -0300

    update procfile file

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,3 @@
 import { serverHttp } from "./app";
 
-serverHttp.listen(process.env.APP_URL || "4000", () => console.log('wello word!'))
+serverHttp.listen(process.env.PORT || "4000", () => console.log('wello word!'))
